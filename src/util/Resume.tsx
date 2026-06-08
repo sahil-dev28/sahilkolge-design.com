64e77208c9dc3b47c0afacbb923c2af7bea18bf3
sahil-dev28 <sahilkolge28@gmail.com>
2026-06-08 12:59:02 +0530

    Change the resume URL to point to the Google Drive folder instead of a specific file, allowing for easier access and updates to the resume.

--- src/util/Resume.tsx
+++ src/util/Resume.tsx
@@ -1,4 +1,4 @@
 export const resumeConfig = {
   // url: "https://drive.google.com/file/d/1vT5XC45gKf8-aFMPrujEeBnCsWFWDlcb/preview",
-  url: "https://drive.google.com/file/d/14UTT-lesj527bpPeuPSA2by6v0Oto8L_/view",
+  url: "https://drive.google.com/file/d/14UTT-lesj527bpPeuPSA2by6v0Oto8L_/preview",
 };
